cbd17809d0583cacd45c01e55243074f935400ec
alejo2191 <53320398+alejo2191@users.noreply.github.com>
2019-07-25 18:52:33 -0500

    Add files via upload

---
+++ impresion.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 21:26:05 2019
+
+@author: jairo
+"""
+print('hola mundo')
+#%%
+print('voy a ser un teso\t en python')
+#%%
+print("hola"+" mundo")
+#%%
+print("nombres\t edad\t prenda\nmiguel\t 11\t azul\nluna\t 13\t roja\n"+
+      "alejo\t 13\t verde")
+#%%
